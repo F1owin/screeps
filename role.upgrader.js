afd0fed45b8e3e811db41fbb2f9c3847055b2073
Florian Nehmer <florian.nehmer@posteo.de>
2019-12-12 01:39:23 +0100

    Fixed source allocation for upgrader

--- role.upgrader.js
+++ role.upgrader.js
@@ -18,9 +18,10 @@ var roleUpgrader = {
             }
         }
         else {
+            var sourceIndex = creep.memory.sourceIndex;
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            if(creep.harvest(sources[sourceIndex]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[sourceIndex], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
     }
